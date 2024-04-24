0e893687699616049c5e953fa915a8b9ea9685ae
Mel Plens <melplan572@gmail.com>
2024-04-24 17:03:39 -0300

    update de aulas portfolio

---
+++ JavaScript/mel.js
@@ -0,0 +1,16 @@
+//JAVASCRIPT E FUNDAMENTOS
+// CODIGO INTEIRO COM COMENTARIO, PARA EXECUTAR TIRE AS //
+//EXECUTAR o código usamos no terminal a seguinte escrita:
+//node nomedoarquivo.js
+//Objetos
+const mel = {
+    nome: 'Mel',
+    idade: 19
+    //métodos para objetos
+    ,
+    //métodos para objetos
+    descrever: function () {
+
+        console.log('Meu nome é', $, { this: .nome }, 'e minha idade é', $, { this: .idade });
+    }
+};
